cb4aa05a320ab5693e03ab6a873123e863834a16
Alnur <alnur.tazhbenov@mail.ru>
2025-06-19 22:12:57 +0300

    Use repository for put and delete

--- repositories/base.py
+++ repositories/base.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from sqlalchemy import select, insert
+from sqlalchemy import select, insert, update, delete
 
 
 class BaseRepository:
@@ -19,10 +19,25 @@ class BaseRepository:
         return result.scalars().one_or_none()
 
     async def add(self, data: BaseModel):
-        add_data_stmt = (
+        add_stmt = (
             insert(self.model)
             .values(**data.model_dump())
             .returning(self.model)
         )
-        result = await self.session.execute(add_data_stmt)
+        result = await self.session.execute(add_stmt)
         return result.scalars().one()
+
+    async def edit(self, data: BaseModel, **filter_by):
+        edit_stmt = (
+            update(self.model)
+            .filter_by(**filter_by)
+            .values(**data.model_dump())
+        )
+        await self.session.execute(edit_stmt)
+
+    async def delete(self, **filter_by):
+        delete_stmt = (
+            delete(self.model)
+            .filter_by(**filter_by)
+        )
+        await self.session.execute(delete_stmt)

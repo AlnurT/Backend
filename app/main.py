0bc2393b79ac4b38e1582015017a412a6bc4e60d
Alnur <alnur.tazhbenov@mail.ru>
2025-07-12 19:31:11 +0300

    add facilities repository, schemas, get and post methods

--- app/main.py
+++ app/main.py
@@ -10,6 +10,7 @@ from app.api.hotels import router as router_hotels
 from app.api.users import router as router_users
 from app.api.rooms import router as router_rooms
 from app.api.bookings import router as router_bookings
+from app.api.facilities import router as router_facilities
 
 
 app = FastAPI()
@@ -17,6 +18,7 @@ app.include_router(router_users)
 app.include_router(router_hotels)
 app.include_router(router_rooms)
 app.include_router(router_bookings)
+app.include_router(router_facilities)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)

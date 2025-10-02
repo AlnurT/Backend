879c82f9e3fceb2e4ff1e356cc2eb382662b9624
Alnur <alnur.tazhbenov@mail.ru>
2025-10-02 15:26:26 +0300

    add fixtures for register user, add hotels and rooms

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,58 @@
+import json
+
 import pytest
+from httpx import AsyncClient, ASGITransport
 
 from app.config import settings
-from app.database import engine_null_pool, Base
+from app.database import engine_null_pool, Base, async_session_maker_null_pool
+from app.main import app
 from app.models import *
+from app.schemas.hotels import HotelAdd
+from app.schemas.rooms import RoomAdd
+from app.utils.db_manager import DBManager
 
 
 @pytest.fixture(scope="session", autouse=True)
-async def check_mode():
+async def check_test_mode():
     assert settings.MODE == "TEST"
 
 
 @pytest.fixture(scope="session", autouse=True)
-async def async_main(check_mode):
+async def setup_database(check_test_mode):
     async with engine_null_pool.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def add_mock_hotels(setup_database):
+    with open("tests/mock_hotels.json", "r", encoding="utf-8") as file:
+        hotels_list: list[dict] = json.load(file)
+        hotels_models = [HotelAdd(**hotel) for hotel in hotels_list]
+
+        async with DBManager(session_factory=async_session_maker_null_pool) as db:
+            await db.hotels.add_bulk(hotels_models)
+            await db.commit()
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def add_mock_rooms(setup_database):
+    with open("tests/mock_rooms.json", "r", encoding="utf-8") as file:
+        rooms_list: list[dict] = json.load(file)
+        rooms_models = [RoomAdd(**room) for room in rooms_list]
+
+        async with DBManager(session_factory=async_session_maker_null_pool) as db:
+            await db.rooms.add_bulk(rooms_models)
+            await db.commit()
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def register_user(setup_database):
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as client:
+        await client.post(
+            "/auth/register",
+            json={
+                "email": "alnur@mail.ru",
+                "password": "12345",
+            }
+        )
--- tests/integration_tests/hotels/test_db.py
+++ tests/integration_tests/hotels/test_db.py
@@ -6,5 +6,5 @@ from app.utils.db_manager import DBManager
 async def test_add_hotel():
     hotel_data = HotelAdd(title="Дракон", location="Пекин")
     async with DBManager(session_factory=async_session_maker_null_pool) as db:
-        new_hotel_data = await db.hotels.add(hotel_data)
+        await db.hotels.add(hotel_data)
         await db.commit()

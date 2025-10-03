c9afe99d94ced5f23b000ad5b8f6c77f9d9c8474
Alnur <alnur.tazhbenov@mail.ru>
2025-10-03 17:27:06 +0300

    edit api facilities tests

--- tests/integration_tests/facilities/test_api.py
+++ tests/integration_tests/facilities/test_api.py
@@ -2,19 +2,22 @@ from httpx import AsyncClient
 
 
 async def test_post_facilities(ac: AsyncClient):
+    facility_name = "Джакузи"
     response = await ac.post(
         "/facilities",
         json={
-            "facility_data": {
-                "title": "Джакузи",
-            }
+            "title": facility_name,
         }
     )
+    res = response.json()
     assert response.status_code == 200
+
+    assert isinstance(res, dict)
+    assert "data" in res
+    assert res["data"]["title"] == facility_name
 
 
 async def test_get_facilities(ac: AsyncClient):
-    response = await ac.get(
-        "/facilities",
-    )
+    response = await ac.get("/facilities")
     assert response.status_code == 200
+    assert isinstance(response.json(), list)

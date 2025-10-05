06761f1a381a13eee735832947d61a759068b63c
Alnur <alnur.tazhbenov@mail.ru>
2025-10-05 14:44:21 +0300

    add mock for cache

--- app/repositories/utils.py
+++ app/repositories/utils.py
@@ -54,7 +54,7 @@ def rooms_ids_for_booking(
         .select_from(rooms_left_table)
         .filter(
             rooms_left_table.c.rooms_left > 0,
-            rooms_left_table.c.room_id.in_(rooms_ids_for_hotel)
+            rooms_left_table.c.room_id.in_(select(rooms_ids_for_hotel))
         )
     )
 
